d987b751cc817051fe2b5f32970fc97f60a3ea6f
lavikara <karatemitayo@gmail.com>
2026-06-30 20:16:33 +0100

    implement sidebar layout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { SidebarProvider } from "@/contexts/sidebar-context";
+import { AppShell } from "@/components/layout/app-shell";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppShell>{children}</AppShell>
+    </SidebarProvider>
+  );
+}
