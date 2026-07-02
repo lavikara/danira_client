77542195ff4705c474a3f357dc269ac554906364
lavikara <karatemitayo@gmail.com>
2026-07-02 04:49:46 +0100

    format file

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-   allowedDevOrigins: ['192.168.100.78','192.168.56.1'],
+  allowedDevOrigins: ['192.168.100.78', '192.168.56.1'],
 };
 
 export default nextConfig;
