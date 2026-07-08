bb86c0d5f10e0242f557982404dc63ea79195e8c
lavikara <karatemitayo@gmail.com>
2026-07-08 05:36:26 +0100

    change layout to client component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,18 +1,12 @@
+'use client';
+
 import { SidebarProvider } from '@/contexts/sidebar-context';
-import { AuthProvider } from '@/contexts/auth-context';
-import ClientRouteGuard from '@/components/auth/client-route-guard';
-import { getLoggedInUserAction } from '@/app/(dashboard)/actions/userActions';
 import { AppShell } from '@/components/layout/app-shell';
 
-export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  // const loggedInUser = await getLoggedInUserAction();
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    // <AuthProvider>
-    //   <ClientRouteGuard>
     <SidebarProvider>
       <AppShell>{children}</AppShell>
     </SidebarProvider>
-    //   </ClientRouteGuard>
-    // </AuthProvider>
   );
 }
